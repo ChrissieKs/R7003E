998d571248c85946a9c2891632208d7e9f000af1
Christina K <chrissie95@live.se>
2018-12-17 13:23:52 +0100

    Update LabB.m

--- LabB.m
+++ LabB.m
@@ -21,7 +21,7 @@ desiredPoles = [currentPoles(2) currentPoles(4) -2 -3]
 %adding a feedback K
 
 
-%syms v K1 K2 K3 K4 b1 b2 b3 b4   %Överger standard sättet för ackerman!
+%syms v K1 K2 K3 K4 b1 b2 b3 b4   %Ã–verger standard sÃ¤ttet fÃ¶r ackerman!
 %Asym =sym('a',[4,4])
 %Ksym = [K1 K2 K3 K4]
 %Bsym = [b1; b2; b3; b4]
@@ -139,37 +139,6 @@ Ld = (place(Ad',Cd',poles_d_Ld))'
 
 %see LabB_ObserverOverSimulator_Discrete_Parameters.m 
 % :P 
-
-
-
-%%
-%testa some copy pasta från tidigare!
-
-
-C1 = [5 1 7 3.5] %try to find some good roots from here
- [num ,den] = ss2zp(Ad,Bd,C1,D,1)
-%[num den] = ss2tf(Ad,Bd,C1,D,1)  %new tf due to new C...
-%num = real(num)
-%[z,p,gain ]  = ss2zp(A, B, C1, D, 1) %find all the zeros/poles!
-
-
-%find D(s) N(s) and plot Root Locus
-s = tf('s');
-Ds = den(1)*s^3+den(2)*s^2+den(3)*s+den(4)
-Ns =num(1)*s^2+num(2)*s^1+num(3)
-
-Dsmin = -den(1)*s^3+den(2)*s^2-den(3)*s+den(4)
-Nsmin =num(1)*s^2-num(2)*s^1+num(3)
-
-
-%for 5 s'es
-%Ds = den(1)*s^4+den(2)*s^3+den(3)*s^2+den(4)*s+den(5)
-%Ns =num(1)*s^4+num(2)*s^3+num(3)*s^2+num(4)*s+num(5)
-%Dsmin = den(1)*s^4-den(2)*s^3+den(3)*s^2-den(4)*s+den(5)
-%Nsmin = num(1)*s^4-num(2)*s^3+num(3)*s^2-num(4)*s+num(5)
-
-rlocus(Ns*Nsmin/(Ds*Dsmin))
-%axis([-10 10 -5 5])
 
 %more task 4.7
 %acker osv
